a5c1579c78965a626fc567b633bfde9cf88bdc51
slavizdravkov <slavizdravkov@gmail.com>
2019-03-21 21:17:37 +0200

    Display answers for question

--- app/User.php
+++ app/User.php
@@ -46,4 +46,13 @@ class User extends Authenticatable
     {
         return $this->hasMany(Answer::class);
     }
+
+    public function getAvatarAttribute()
+    {
+        $email = $this->email;
+        $size = 40;
+
+        return "https://www.gravatar.com/avatar/" . md5( strtolower( trim( $email ) ) ) . "?&s=" . $size;
+
+    }
 }
